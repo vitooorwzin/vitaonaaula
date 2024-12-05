5ec8252fb5eb5eb6b87de149c20de6d74479c6d3
vitooorwzin <165226747+vitooorwzin@users.noreply.github.com>
2024-12-05 01:25:00 -0300

    dse

---
+++ script.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector('.sidebar');
+const toggleButton = document.querySelector('.toggle-button');
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
